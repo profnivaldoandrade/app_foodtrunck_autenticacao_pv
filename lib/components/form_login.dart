e57c7f79a5e6639a841512cb96727b4e638b481f
profnivaldoandrade <nivaldo.andrade@gmail.com>
2023-04-28 14:37:42 -0300

    atualização 2

--- lib/components/form_login.dart
+++ lib/components/form_login.dart
@@ -10,12 +10,31 @@ class FormLogin extends StatefulWidget {
 }
 
 class _FormLoginState extends State<FormLogin> {
-  final ModoLogin _modoLogin = ModoLogin.Login;
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _estaLogando = false;
+  ModoLogin _modoLogin = ModoLogin.Login;
   Map<String, String> _dadosLogin = {
     'email': '',
     'password': '',
   };
-  void _submit() {}
+
+  void _auternarModoLogin() {
+    setState(() {
+      if (_eLogin()) {
+        _modoLogin = ModoLogin.Registrar;
+      } else {
+        _modoLogin = ModoLogin.Login;
+      }
+    });
+  }
+
+  void _submit() {
+    setState(() => _estaLogando = true);
+  }
+
+  bool _eLogin() => _modoLogin == ModoLogin.Login;
+  bool _eRegistro() => _modoLogin == ModoLogin.Registrar;
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +46,9 @@ class _FormLoginState extends State<FormLogin> {
       child: Container(
         padding: const EdgeInsets.all(16),
         width: tamanhoTela.width * 0.75,
+        height: _eLogin() ? 340 : 400,
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -35,26 +56,60 @@ class _FormLoginState extends State<FormLogin> {
                     labelText: 'E-mail', icon: Icon(Icons.email)),
                 keyboardType: TextInputType.emailAddress,
                 onSaved: (email) => _dadosLogin['email'] = email ?? '',
+                validator: (_email) {
+                  final email = _email ?? '';
+                  if (email.trim().isEmpty || !email.contains('@')) {
+                    return 'Informar um e-mail valido';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 decoration: const InputDecoration(
                     labelText: 'Senha', icon: Icon(Icons.key)),
                 keyboardType: TextInputType.text,
                 obscureText: true,
-                onSaved: (password) => _dadosLogin['passeord'] == password ?? '',
+                controller: _passwordController,
+                onSaved: (password) =>
+                    _dadosLogin['passeord'] == password ?? '',
+                validator: (_password) {
+                  final password = _password ?? '';
+                  if (password.isEmpty || password.length < 5) {
+                    return 'Informar senha minimo 5 caracteres';
+                  }
+                  return null;
+                },
               ),
-              if (_modoLogin == ModoLogin.Registrar)
+              if (_eRegistro())
                 TextFormField(
                   decoration: const InputDecoration(
                       labelText: 'Confirmar Senha', icon: Icon(Icons.key)),
                   keyboardType: TextInputType.text,
                   obscureText: true,
+                  validator: (_password) {
+                    final password = _password ?? '';
+                    if (password != _passwordController.text) {
+                      return 'Senha informadas não conferem...';
+                    }
+                    return null;
+                  },
                 ),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _submit,
+              if (_estaLogando)
+                const CircularProgressIndicator()
+              else
+                ElevatedButton(
+                  onPressed: _submit,
+                  child: Text(
+                    _eLogin() ? 'ENTRAR' : 'REGISTRAR',
+                  ),
+                ),
+              const Spacer(),
+              TextButton(
+                onPressed: _auternarModoLogin,
                 child: Text(
-                  _modoLogin == ModoLogin.Login ? 'ENTRAR' : 'REGISTRAR',
+                  _eLogin() ? 'DESEJA REGISTRAR' : 'JÁ POSSUI CONTA?',
+                  style: Theme.of(context).textTheme.labelLarge,
                 ),
               ),
             ],
